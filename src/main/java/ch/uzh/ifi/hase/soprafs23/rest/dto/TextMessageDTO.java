06905be359150cecd8898643dd968fd4fd49e9eb
stefanpluess <stefan.pluess@uzh.ch>
2023-04-25 18:03:18 +0200

    implemented basic websocket

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/TextMessageDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs23.rest.dto;
+
+public class TextMessageDTO {
+
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
