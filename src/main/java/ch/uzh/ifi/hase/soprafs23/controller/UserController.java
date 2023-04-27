6a052c677adf26ed0552ab6702ce8fbeae5717c0
stefanpluess <stefan.pluess@uzh.ch>
2023-04-27 15:55:28 +0200

    removed duplicate declaration of variable

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/UserController.java
@@ -39,8 +39,7 @@ public class UserController {
 //    this.roomService = roomService;
   }
 
-@Autowired
-SimpMessagingTemplate template;
+
 
   @GetMapping("/users")
   @ResponseStatus(HttpStatus.OK)
