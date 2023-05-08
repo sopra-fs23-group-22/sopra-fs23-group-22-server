900dfb2cb1e20433eebf229211865ea3be69521e
stefanpluess <91138249+stefanpluess@users.noreply.github.com>
2023-05-09 01:44:17 +0200

    Added RoomTest.java

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/game/RoomTest.java
@@ -0,0 +1,42 @@
+package ch.uzh.ifi.hase.soprafs23.game;
+
+import ch.uzh.ifi.hase.soprafs23.game.states.GameState;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class RoomTest {
+
+
+    @Test
+    public void testEnterGame(){
+        //create room
+        int id = 4;
+        Room room = new Room(id);
+        ArrayList<Long> userIds = new ArrayList<>();
+        userIds.add(1L);
+        userIds.add(5L);
+        room.setUserIds(userIds);
+
+
+        room.enterGame();
+
+        assertEquals(room.getGame().getGameState(), GameState.PRE_PLAY);
+    }
+
+    @Test
+    public void testEnterGameException(){
+        int id = 3;
+        Room room = new Room(id);
+        ArrayList<Long> userIds = new ArrayList<>();
+        userIds.add(2L);
+        room.setUserIds(userIds);
+
+        assertThrows(IllegalStateException.class , room::enterGame);
+    }
+}
