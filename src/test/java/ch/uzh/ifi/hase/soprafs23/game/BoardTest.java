aded16805912d308d6dd1605ebb19b9ee9b45b79
stefanpluess <91138249+stefanpluess@users.noreply.github.com>
2023-05-21 02:06:01 +0200

    Updated BoardTest.java to recent changes

--- src/test/java/ch/uzh/ifi/hase/soprafs23/game/BoardTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/game/BoardTest.java
@@ -13,6 +13,10 @@ import ch.uzh.ifi.hase.soprafs23.game.piece.movestrategies.MoveResult;
 import ch.uzh.ifi.hase.soprafs23.game.states.AliveState;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
@@ -223,4 +227,113 @@ public class BoardTest {
             }
         }
     }
+
+    private Board setupAvailableTargets(){
+        Board board = new Board();
+        Piece bombBlue = new Piece(PieceType.BOMB, ArmyType.BLUE);
+        Piece flagBlue = new Piece(PieceType.FLAG, ArmyType.BLUE);
+        Piece scoutBlue = new Piece(PieceType.SCOUT, ArmyType.BLUE);
+        Piece captainBlue = new Piece(PieceType.CAPTAIN, ArmyType.BLUE);
+        Piece generalBlue = new Piece(PieceType.GENERAL, ArmyType.BLUE);
+        Piece bombRed1 = new Piece(PieceType.BOMB, ArmyType.RED);
+        Piece bombRed2 = new Piece(PieceType.BOMB, ArmyType.RED);
+        board.setPiece(0, 5, bombBlue);
+        board.setPiece(1, 5, captainBlue);
+        board.setPiece(1,6, bombRed1);
+        board.setPiece(0,7,generalBlue);
+        board.setPiece(3,7,scoutBlue);
+        board.setPiece(8,7,bombRed2);
+
+        /*This is how the set up board looks like:
+x - empty squares, L - lake squares, B - blue bombs, C - blue captain, G - blue general, S - blue scout, R - Red Pieces
+            xxxxxxxxxx
+            xxxxxxxxxx
+            xxxxxxxxxx
+            xxxxxxxxxx
+            xxLLxxLLxx
+            BCLLxxLLxx
+            xRxxxxxxxx
+            GxxSxxxxRx
+            xxxxxxxxxx
+            xxxxxxxxxx
+        */
+        return board;
+    }
+
+    @Test
+    public void test_getAvailableTargets(){
+        //setup Board
+        Board board = setupAvailableTargets();
+        //getAvailableTargets for the bomb should return empty list
+        List<Square> expected = new ArrayList<>();
+        ArrayList<Square> actual = board.getAvailableTargets(new Axis[]{Axis._5, Axis._0});
+        assertEquals(expected, actual);
+        //getAvailableTargets for the captain should return Axis (6,1), (4, 1)
+        expected = Arrays.asList(board.getSquare(4, 1), board.getSquare(6, 1));
+        actual = board.getAvailableTargets(new Axis[]{Axis._5, Axis._1});
+        assertTrue(actual.containsAll(expected) && actual.size() == expected.size());
+        //getAvailableTargets for the general should return Axis (6,0), (7,1), (8,0)
+        expected = Arrays.asList(board.getSquare(6,0), board.getSquare(7,1), board.getSquare(8,0));
+        actual = board.getAvailableTargets(new Axis[]{Axis._7, Axis._0});
+        assertTrue(actual.containsAll(expected) && actual.size() == expected.size());
+        //getAvailableTargets for the scout should return Axis (7,1),(7,2),(6,3),(8,3),(9,3),(7,4),(7,5),(7,6),(7,7),(7,8)
+        expected = Arrays.asList(board.getSquare(7,1),board.getSquare(7,2),board.getSquare(6,3),board.getSquare(8,3),board.getSquare(9,3), board.getSquare(7,4),board.getSquare(7,5),board.getSquare(7,6),board.getSquare(7,7),board.getSquare(7,8));
+        actual = board.getAvailableTargets(new Axis[]{Axis._7, Axis._3});
+        assertTrue(actual.containsAll(expected) && actual.size() == expected.size());
+    }
+
+    @Test
+    public void test_getSurroundingSquaresViaAxis(){
+        //Since this is a private method we will test it Via getAvailableTargets
+        //There are 9 distinct cases to cover so we will create 9 pieces
+        Board board = new Board();
+        Piece piece = new Piece(PieceType.SERGEANT, ArmyType.BLUE);
+        board.setPiece(0, 0, piece);
+        board.setPiece(9, 0, piece);
+        board.setPiece(0, 9, piece);
+        board.setPiece(9, 9, piece);
+        board.setPiece(0, 7, piece);
+        board.setPiece(7, 0, piece);
+        board.setPiece(7, 9, piece);
+        board.setPiece(9, 7, piece);
+        board.setPiece(2, 2, piece);
+        //case 1: x=0, y=0
+        List<Square> actual = board.getAvailableTargets(new Axis[]{Axis._0, Axis._0});
+        List<Square> expected = Arrays.asList(board.getSquare(0, 1), board.getSquare(1, 0));
+        assertTrue(actual.size() == 2 && actual.containsAll(expected));
+        //case 2: x=0, y=9
+        actual = board.getAvailableTargets(new Axis[]{Axis._0, Axis._9});
+        expected = Arrays.asList(board.getSquare(1, 9), board.getSquare(0, 8));
+        assertTrue(actual.size() == 2 && actual.containsAll(expected));
+        //case 3: x=9, y=0
+        actual = board.getAvailableTargets(new Axis[]{Axis._9, Axis._0});
+        expected = Arrays.asList(board.getSquare(9, 1), board.getSquare(8, 0));
+        assertTrue(actual.size() == 2 && actual.containsAll(expected));
+        //case 4: x=9, y=9
+        actual = board.getAvailableTargets(new Axis[]{Axis._9, Axis._9});
+        expected = Arrays.asList(board.getSquare(8, 9), board.getSquare(9, 8));
+        assertTrue(actual.size() == 2 && actual.containsAll(expected));
+        //case 5: x=0, y=7
+        actual = board.getAvailableTargets(new Axis[]{Axis._0, Axis._7});
+        expected = Arrays.asList(board.getSquare(1, 7), board.getSquare(0, 6), board.getSquare(0, 8));
+        assertTrue(actual.size() == 3 && actual.containsAll(expected));
+        //case 6: x=9, y=7
+        actual = board.getAvailableTargets(new Axis[]{Axis._9, Axis._7});
+        expected = Arrays.asList(board.getSquare(8, 7), board.getSquare(9, 6), board.getSquare(9, 8));
+        assertTrue(actual.size() == 3 && actual.containsAll(expected));
+        //case 7: x=7, y=0
+        actual = board.getAvailableTargets(new Axis[]{Axis._7, Axis._0});
+        expected = Arrays.asList(board.getSquare(7, 1), board.getSquare(6, 0), board.getSquare(8, 0));
+        assertTrue(actual.size() == 3 && actual.containsAll(expected));
+        //case 8: x=7, y=9
+        actual = board.getAvailableTargets(new Axis[]{Axis._7, Axis._9});
+        expected = Arrays.asList(board.getSquare(7, 8), board.getSquare(6, 9), board.getSquare(8, 9));
+        assertTrue(actual.size() == 3 && actual.containsAll(expected));
+        //case 9: x=2, y=2
+        actual = board.getAvailableTargets(new Axis[]{Axis._2, Axis._2});
+        expected = Arrays.asList(board.getSquare(2, 1), board.getSquare(1, 2), board.getSquare(2, 3), board.getSquare(3, 2));
+        assertTrue(actual.size() == 4 && actual.containsAll(expected));
+    }
+
+
 }
